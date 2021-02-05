6368b66fe9cc96c434562d131b98d9251ffa18ff
Iraj Patel <irajtheindian@gmail.com>
2021-02-05 20:56:56 +0000

    got rid of prompt() and added start script

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const Blackjack = require('./src/Blackjack')
+const readline = require('readline');
+
+function ask(question) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    return new Promise(resolve => rl.question(question, answer => {
+        rl.close();
+        resolve(answer);
+    }))
+}
 
 // Fetch list of available games, allow user to pick one and load it.
 
@@ -7,18 +20,24 @@ const Blackjack = require('./src/Blackjack')
    "2": "Poker"
  }
 
-for(key in numberToGame) {
-  console.log(`${key}) ${numberToGame[key]}`)
+var userGameNumberChoice
+
+async function getGameChoice() {
+  for(key in numberToGame) {
+    console.log(`${key}) ${numberToGame[key]}`)
+  }
+
+  userGameNumberChoice = await ask('Enter your choice ')
+
+  while(!numberToGame.hasOwnProperty(userGameNumberChoice)) {
+    console.log("Uh-Oh, that is not a valid choice, try again!")
+    var userGameNumberChoice = await ask('Enter your choice ')
+  }
+
+  if(numberToGame[userGameNumberChoice] == "Blackjack") {
+    let game = new Blackjack()
+    game.play()
+  }
 }
 
-var userGameNumberChoice = prompt('Enter your choice ')
-
-while(!numberToGame.hasOwnProperty(userGameNumberChoice)) {
-  console.log("Uh-Oh, that is not a valid choice, try again!")
-  var userGameNumberChoice = prompt('Enter your choice ')
-}
-
-if(numberToGame[userGameNumberChoice] == "Blackjack") {
-  let game = new Blackjack()
-  game.play()
-}
+getGameChoice()
